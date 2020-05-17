6d7d57926bfa2cde31301a892966b98c3d53dc0a
Sami Kurvinen <sami.kurvinen@gmail.com>
2020-05-17 09:49:07 +0300

    Updates repo in its own function

--- get-commits.sh
+++ get-commits.sh
@@ -9,25 +9,30 @@ COMMIT_AUTHOR=${2}
 SINCE=${3:-"4.days.ago"}
 UPDATE_REPO=${4:-false}
 
-function getCommitsForAuthor() {
-  REPOSITORY_PATH=$1
-  AUTHOR=$2
-
-  cd "${REPOSITORY_PATH}"
-
-  if [ "${UPDATE_REPO}" == "true" ] ; then
-    git fetch --all > /dev/null
-  fi
-
-  git shortlog --all --since="${SINCE}" --pretty=format:"%ai %d %s" --reverse --date=format:"%H" --author="${AUTHOR}"
-}
-
 function printCommits() {
   printf "\n## ${1}: \n\n"
   printf "${2}\n"
 }
 
+function getCommitsForAuthor() {
+  REPOSITORY_PATH=$1
+  AUTHOR=$2
+
+  cd "${REPOSITORY_PATH}" && git shortlog --all --since="${SINCE}" --pretty=format:"%ai %d %s" --reverse --date=format:"%H" --author="${AUTHOR}"
+}
+
+function updateRepo() {
+  cd ${1}
+
+  if [ "${UPDATE_REPO}" == "true" ] ; then
+    git fetch --all > /dev/null
+  fi
+
+  cd ..
+}
+
 for repositoryPath in "${REPOSITORY_PATHS[@]}"; do
+  updateRepo "${repositoryPath}"
   COMMITS=$(getCommitsForAuthor "${repositoryPath}" "${COMMIT_AUTHOR}")
   printCommits "${repositoryPath}" "${COMMITS}"
 done
